d3f5db14aca8a5de1d8cbe83fabf83caced13a9a
chanoch shechter <chashe21@gmail.com>
2020-08-16 23:44:51 +0300

    Updates

---
+++ precache-manifest.f7c5d2caa5490ec07d540cd4990cccd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af739a59c7bc89c84629cf676299238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70db05b99680fb18c46",
+    "url": "/static/css/main.f1b37401.chunk.css"
+  },
+  {
+    "revision": "eceaa433cf8da1b0c1d9",
+    "url": "/static/js/2.7a42305a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a42305a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70db05b99680fb18c46",
+    "url": "/static/js/main.ab60df86.chunk.js"
+  },
+  {
+    "revision": "402f05c59b4e2a57dec5",
+    "url": "/static/js/runtime-main.d1310a82.js"
+  }
+]);
